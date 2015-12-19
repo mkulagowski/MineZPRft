mkulagowski/MineZPRaft

Swap todo comments with LOG macros
Swaps all "// TODO log" comments with proper LOG macros.

Verification: Build and run. Everything You see in console should be some
type of LOG.

MineZPRft/Renderer/Renderer.cpp
@@ -9,6 +9,7 @@
 #include "Common/Common.hpp"
 #include "Extensions.hpp"
 #include "Math/Vector.hpp"
+#include "Common/Logger.hpp"
 
 #include <iostream>
 
@@ -39,8 +40,7 @@ void Renderer::Init(const RendererDesc& desc)
 {
     const GLubyte* glv = glGetString(GL_VERSION);
     std::string glvStr(reinterpret_cast<const char*>(glv));
-    // TODO log
-    std::cerr << "Loading OpenGL " << glvStr << " Renderer" << std::endl;
+    LOG_INFO("Loading OpenGL " << glvStr << " Renderer" << std::endl);
 
     // initialize OpenGL extensions
     if (!ExtensionsInit())

MineZPRft/Renderer/Shader.cpp
@@ -7,6 +7,7 @@
 #include "Shader.hpp"
 
 #include "Extensions.hpp"
+#include "Common/Logger.hpp"
 
 #include <fstream>
 #include <vector>
@@ -61,8 +62,7 @@ void Shader::Init(const ShaderDesc& desc)
     glGetProgramiv(mShaderProgram, GL_LINK_STATUS, &programStatus);
     if (!programStatus)
     {
-        // TODO log
-        std::cerr << "Shader Program failed to link." << std::endl;
+        LOG_ERROR("Shader Program failed to link." << std::endl);
 
         int logLength = 0;
         glGetProgramiv(mShaderProgram, GL_INFO_LOG_LENGTH, &logLength);
@@ -71,15 +71,15 @@ void Shader::Init(const ShaderDesc& desc)
         {
             std::vector<char> log(logLength);
             glGetProgramInfoLog(mShaderProgram, logLength, &logLength, log.data());
-            std::cerr << "Shader Program linking output:\n" << log.data() << std::endl;
+            LOG_INFO("Shader Program linking output:\n" << log.data()
+                     << std::endl);
         }
 
         // TODO exception
         return;
     }
 
-    // TODO log info
-    std::cerr << "Successfully compiled and linked shader." << std::endl;
+    LOG_INFO("Successfully compiled and linked shader." << std::endl);
 }
 
 void Shader::MakeCurrent() noexcept
@@ -103,8 +103,7 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
     std::ifstream shaderFile(shaderPath);
     if (!shaderFile)
     {
-        // TODO log
-        std::cerr << "Invalid shader file: " << shaderPath << std::endl;
+        LOG_ERROR("Invalid shader file: " << shaderPath << std::endl);
         // TODO exception
         return GL_NONE;
     }
@@ -116,8 +115,8 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
     GLuint shader = glCreateShader(shaderType);
     if (!shader)
     {
-        // TODO log
-        std::cerr << "Failed to create Shader Object for " << shaderPath << std::endl;
+        LOG_ERROR("Failed to create Shader Object for " << shaderPath
+                  << std::endl);
         // TODO exception
         return GL_NONE;
     }
@@ -130,8 +129,7 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
     glGetShaderiv(shader, GL_COMPILE_STATUS, &shaderStatus);
     if (!shaderStatus)
     {
-        // TODO log
-        std::cerr << "Failed to compile shader " << shaderPath << std::endl;
+        LOG_ERROR("Failed to compile shader " << shaderPath << std::endl);
 
         int logLength = 0;
         glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
@@ -141,17 +139,15 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
             std::vector<char> log(logLength);
             glGetShaderInfoLog(shader, logLength, &logLength, log.data());
 
-            // TODO log
-            std::cerr << "Shader '" << shaderPath << "' compilation output:\n" << log.data()
-                      << std::endl;
+            LOG_INFO("Shader '" << shaderPath << "' compilation output:\n"
+                     << log.data() << std::endl);
         }
 
         // TODO exception
         return GL_NONE;
     }
 
-    // TODO log info
-    std::cerr << "Compiled Shader " << shaderPath << std::endl;
+    LOG_INFO("Compiled Shader " << shaderPath << std::endl);
 
     return shader;
 }

mkulagowski/MineZPRft

Change-Id: Ic40040783a72c7cd4902f40d724f104a17460a74

MineZPRft/Renderer/Renderer.cpp
@@ -9,6 +9,7 @@
 #include "Common/Common.hpp"
 #include "Extensions.hpp"
 #include "Math/Vector.hpp"
+#include "Common/Logger.hpp"
 
 #include <iostream>
 
@@ -43,8 +44,7 @@ void Renderer::Init(const RendererDesc& desc)
 {
     const GLubyte* glv = glGetString(GL_VERSION);
     std::string glvStr(reinterpret_cast<const char*>(glv));
-    // TODO log
-    std::cerr << "Loading OpenGL " << glvStr << " Renderer" << std::endl;
+    LOG_I("Loading OpenGL " << glvStr << " Renderer" << std::endl);
 
     // initialize OpenGL extensions
     if (!ExtensionsInit())

MineZPRft/Renderer/Shader.cpp
@@ -7,6 +7,7 @@
 #include "Shader.hpp"
 
 #include "Extensions.hpp"
+#include "Common/Logger.hpp"
 
 #include <fstream>
 #include <vector>
@@ -61,8 +62,7 @@ void Shader::Init(const ShaderDesc& desc)
     glGetProgramiv(mShaderProgram, GL_LINK_STATUS, &programStatus);
     if (!programStatus)
     {
-        // TODO log
-        std::cerr << "Shader Program failed to link." << std::endl;
+        LOG_E("Shader Program failed to link." << std::endl);
 
         int logLength = 0;
         glGetProgramiv(mShaderProgram, GL_INFO_LOG_LENGTH, &logLength);
@@ -71,15 +71,15 @@ void Shader::Init(const ShaderDesc& desc)
         {
             std::vector<char> log(logLength);
             glGetProgramInfoLog(mShaderProgram, logLength, &logLength, log.data());
-            std::cerr << "Shader Program linking output:\n" << log.data() << std::endl;
+            LOG_I("Shader Program linking output:\n" << log.data()
+                     << std::endl);
         }
 
         // TODO exception
         return;
     }
 
-    // TODO log info
-    std::cerr << "Successfully compiled and linked shader." << std::endl;
+    LOG_I("Successfully compiled and linked shader." << std::endl);
 }
 
 void Shader::MakeCurrent() noexcept
@@ -103,8 +103,7 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
     std::ifstream shaderFile(shaderPath);
     if (!shaderFile)
     {
-        // TODO log
-        std::cerr << "Invalid shader file: " << shaderPath << std::endl;
+        LOG_E("Invalid shader file: " << shaderPath << std::endl);
         // TODO exception
         return GL_NONE;
     }
@@ -116,8 +115,8 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
     GLuint shader = glCreateShader(shaderType);
     if (!shader)
     {
-        // TODO log
-        std::cerr << "Failed to create Shader Object for " << shaderPath << std::endl;
+        LOG_E("Failed to create Shader Object for " << shaderPath
+                  << std::endl);
         // TODO exception
         return GL_NONE;
     }
@@ -130,8 +129,7 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
     glGetShaderiv(shader, GL_COMPILE_STATUS, &shaderStatus);
     if (!shaderStatus)
     {
-        // TODO log
-        std::cerr << "Failed to compile shader " << shaderPath << std::endl;
+        LOG_E("Failed to compile shader " << shaderPath << std::endl);
 
         int logLength = 0;
         glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
@@ -141,17 +139,15 @@ GLuint Shader::CreateShader(const GLenum shaderType, const std::string& shaderPa
             std::vector<char> log(logLength);
             glGetShaderInfoLog(shader, logLength, &logLength, log.data());
 
-            // TODO log
-            std::cerr << "Shader '" << shaderPath << "' compilation output:\n" << log.data()
-                      << std::endl;
+            LOG_I("Shader '" << shaderPath << "' compilation output:\n"
+                     << log.data() << std::endl);
         }
 
         // TODO exception
         return GL_NONE;
     }
 
-    // TODO log info
-    std::cerr << "Compiled Shader " << shaderPath << std::endl;
+    LOG_I("Compiled Shader " << shaderPath << std::endl);
 
     return shader;
 }